c1f4594643b9f4058e34bdffac35af58b032406d
Tim Kempkens <Tim.Kempkens@googlemail.com>
2022-05-27 13:09:19 +0200

    added  module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,7 +39,26 @@ import { NgxAuthFirebaseUIModule } from 'ngx-auth-firebaseui';
       apiKey: 'AIzaSyA8nrqEocO9WoXqFDT2MpSMGP9pw1K-XaQ',
       authDomain: 'slack-44d87.firebaseapp.com',
       messagingSenderId: '1091077346802',
-    })
+    },
+    () => 'slack-44d87',
+                {
+                  enableFirestoreSync: true, // enable/disable autosync users with firestore
+                  toastMessageOnAuthSuccess: false, // whether to open/show a snackbar message on auth success - default : true
+                  toastMessageOnAuthError: false, // whether to open/show a snackbar message on auth error - default : true
+                  authGuardFallbackURL: '/loggedout', // url for unauthenticated users - to use in combination with canActivate feature on a route
+                  authGuardLoggedInURL: '/loggedin', // url for authenticated users - to use in combination with canActivate feature on a route
+                  passwordMaxLength: 60, // `min/max` input parameters in components should be within this range.
+                  passwordMinLength: 4, // Password length min/max in forms independently of each componenet min/max.
+                  // Same as password but for the name
+                  nameMaxLength: 50,
+                  nameMinLength: 2,
+                  // If set, sign-in/up form is not available until email has been verified.
+                  // Plus protected routes are still protected even though user is connected.
+                  guardProtectedRoutesUntilEmailIsVerified: true,
+                  enableEmailVerification: false, // default: true
+                  useRawUserCredential: true, // If set to true outputs the UserCredential object instead of firebase.User after login and signup - Default: false
+                }),
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
