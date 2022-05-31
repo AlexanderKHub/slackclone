fed9399a3db2a947b196fd72ab382db27325134e
Simon Baumhauer <simon.baumhauer@hotmail.ch>
2022-05-31 12:40:02 +0200

    thread functionality added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -27,6 +27,7 @@ export class HomeComponent implements OnInit {
   activeThread!: any;
   allThreads!: any;
   currentMessageId!: string;
+  currentMessage!: Message;
 
   constructor(
     private firestore: AngularFirestore,
@@ -36,10 +37,10 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.firestore
       .collection('users')
-      .valueChanges({idField: 'userId'})
+      .valueChanges({ idField: 'userId' })
       .subscribe((changes: any) => {
         this.users = changes;
-      })
+      });
 
     this.route.params.subscribe((params) => {
       this.authorId = params['uid'];
@@ -48,7 +49,7 @@ export class HomeComponent implements OnInit {
         .doc(params['channelid'])
         .valueChanges({ idField: 'channelid' })
         .subscribe((changes: any) => {
-          if(!changes.name) return; 
+          if (!changes.name) return;
           this.activeChannel = changes;
         });
 
@@ -57,7 +58,7 @@ export class HomeComponent implements OnInit {
         .doc(params['channelid'])
         .valueChanges({ idField: 'channelid' })
         .subscribe((changes: any) => {
-          if(!changes.name) return; 
+          if (!changes.name) return;
           this.activeChannel = changes;
         });
 
@@ -66,46 +67,50 @@ export class HomeComponent implements OnInit {
         .valueChanges({ idField: 'messageId' })
         .subscribe((changes: any) => {
           this.messages = changes
-          .filter((message: any) => {
-            return message.channelKey == this.activeChannel.channelid;
-          })
-          .sort((mess1: any, mess2: any) => {
-            return mess1.time - mess2.time;
-          });
+            .filter((message: any) => {
+              return message.channelKey == this.activeChannel.channelid;
+            })
+            .sort((mess1: any, mess2: any) => {
+              return mess1.time - mess2.time;
+            });
           this.loadAllThreads();
         });
-      
+
       this.firestore
-      .collection('users')
-      .valueChanges({idField: 'userId'})
-      .subscribe((changes: any) => {
-        this.users = changes;
-      })
+        .collection('users')
+        .valueChanges({ idField: 'userId' })
+        .subscribe((changes: any) => {
+          this.users = changes;
+        });
     });
   }
 
   loadAllThreads() {
     this.firestore
-    .collection('threads')
-    .valueChanges({idField: 'threadId'})
-    .subscribe((changes: any) => {
-      console.log('Update AllThreads from firebase');
-      
-      this.allThreads = changes.filter((thread: Thread) => {
-        for (let i = 0; i < this.messages.length; i++) {
-          if (this.messages[i].messageId == thread.messageKey) return true;
-        }
-        return false;
+      .collection('threads')
+      .valueChanges({ idField: 'threadId' })
+      .subscribe((changes: any) => {
+        console.log('Update AllThreads from firebase');
+
+        this.allThreads = changes.filter((thread: Thread) => {
+          for (let i = 0; i < this.messages.length; i++) {
+            if (this.messages[i].messageId == thread.messageKey) return true;
+          }
+          return false;
+        });
+        this.findMessageThread(this.currentMessageId);
       });
-      this.findMessageThread(this.currentMessageId);
-    })
   }
 
-  findMessageThread(messageId: string){
-    this.activeThread = this.allThreads.filter((thread: any) => messageId == thread.messageKey);
+  findMessageThread(messageId: string) {
+    this.activeThread = this.allThreads
+      .filter((thread: any) => messageId == thread.messageKey)
+      .sort((mess1: any, mess2: any) => {
+        return mess1.time - mess2.time;
+      });
   }
 
-  getUserById(userId: string){
+  getUserById(userId: string) {
     return this.users.find((user: any) => user.userId == userId);
   }
 
@@ -117,10 +122,11 @@ export class HomeComponent implements OnInit {
     this.firestore.collection('messages').add(newMessage.toJSON());
   }
 
-  openThread(message: any){
+  openThread(message: any) {
     this.findMessageThread(message.messageId);
     this.currentMessageId = message.messageId;
     this.showThread = true;
+    this.currentMessage = message;
   }
 
   sendThreadMessage(message: string) {
@@ -129,7 +135,7 @@ export class HomeComponent implements OnInit {
     newThread.author = this.authorId;
     newThread.messageKey = this.currentMessageId;
     console.log(newThread);
-    
+
     this.firestore.collection('threads').add(newThread.toJSON());
   }
 }
